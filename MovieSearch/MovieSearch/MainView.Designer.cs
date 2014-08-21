786327a91bc121cb6d65348a621a01e1c7c2b10c
Sandrino Di Mattia <contact@sandrino.be>
2014-08-21 21:26:58 +0200

    Initial MovieSearch project

---
+++ MovieSearch/MovieSearch/MainView.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MovieSearch
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.gridView = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textGenre = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Index";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnCreateIndex);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import Movies";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnImportMovies);
+            // 
+            // dataGridView1
+            // 
+            this.gridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Location = new System.Drawing.Point(13, 157);
+            this.gridView.Name = "gridView";
+            this.gridView.RowTemplate.Height = 28;
+            this.gridView.Size = new System.Drawing.Size(880, 481);
+            this.gridView.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnSearch);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(160, 85);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(549, 26);
+            this.textSearch.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Genre";
+            // 
+            // textGenre
+            // 
+            this.textGenre.Location = new System.Drawing.Point(232, 118);
+            this.textGenre.Name = "textGenre";
+            this.textGenre.Size = new System.Drawing.Size(477, 26);
+            this.textGenre.TabIndex = 6;
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 650);
+            this.Controls.Add(this.textGenre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.gridView);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainView";
+            this.Text = "Movie Search";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView gridView;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textGenre;
+    }
+}
+
---
+++ MovieSearch/MovieSearch/MainView.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+using RedDog.Search;
+using RedDog.Search.Http;
+using RedDog.Search.Model;
+
+namespace MovieSearch
+{
+    public partial class MainView : Form
+    {
+        private IndexManagementClient _managementClient;
+
+        private IndexQueryClient _queryClient;
+
+        public MainView()
+        {
+            InitializeComponent();
+            InitSearch();
+        }
+
+        private void InitSearch()
+        {
+            var connection = ApiConnection.Create(ConfigurationManager.AppSettings["Azure.Search.ServiceName"],
+                ConfigurationManager.AppSettings["Azure.Search.ApiKey"]);
+            _managementClient = new IndexManagementClient(connection);
+            _queryClient = new IndexQueryClient(connection);
+        }
+
+        private async void OnCreateIndex(object sender, EventArgs e)
+        {
+            var result = await _managementClient.CreateIndexAsync(new Index("imdb-movies")
+                .WithStringField("id", opt =>
+                    opt.IsKey().IsRetrievable())
+                .WithStringField("title", opt =>
+                    opt.IsRetrievable().IsSearchable())
+                .WithStringField("description", opt =>
+                    opt.IsRetrievable().IsSearchable())
+                .WithDoubleField("rating", opt =>
+                    opt.IsRetrievable().IsFilterable())
+                .WithStringCollectionField("genre", opt =>
+                    opt.IsRetrievable().IsFilterable().IsSearchable())
+                );
+
+            if (result.IsSuccess)
+            {
+                MessageBox.Show("Index created!");
+            }
+            else
+            {
+                MessageBox.Show("Error: " + result.Error.Message);
+            }
+        }
+
+        private async void OnImportMovies(object sender, EventArgs e)
+        {
+            var result = await _managementClient.PopulateAsync("imdb-movies", new[]
+            {
+                new IndexOperation(IndexOperationType.Upload, "id", "tt0133093")
+                    .WithProperty("title", "The Matrix")
+                    .WithProperty("description", "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.")
+                    .WithProperty("rating", 8.7)
+                    .WithProperty("genre", new[] {"Action", "Sci-Fi"}),
+
+                new IndexOperation(IndexOperationType.Upload, "id", "tt0159784")
+                    .WithProperty("title", "Takedown")
+                    .WithProperty("description", "This film is based on the story of the capture of computer hacker Kevin Mitnick.")
+                    .WithProperty("rating", 6.3)
+                    .WithProperty("genre", new[] {"Crime", "Drama", "Thriller"}),
+
+                new IndexOperation(IndexOperationType.Upload, "id", "tt1285016")
+                    .WithProperty("title", "The Social Network")
+                    .WithProperty("description", "Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, but is later sued by two brothers who claimed he stole their idea, and the cofounder who was later squeezed out of the business.")
+                    .WithProperty("rating", 7.8)
+                    .WithProperty("genre", new[] {"Biography", "Drama"})
+            });
+
+            foreach (var record in result.Body.Where(r => !r.Status))
+            {
+                MessageBox.Show(String.Format("Error with record {0}: {1}", record.Key, record.ErrorMessage));
+
+            }
+        }
+
+        private async void OnSearch(object sender, EventArgs e)
+        {
+            var query = new SearchQuery(textSearch.Text);
+            if (!String.IsNullOrWhiteSpace(textGenre.Text))
+                query.Filter = String.Format("genre/any(t: t eq '{0}')", textGenre.Text);
+
+            var result = await _queryClient.SearchAsync("imdb-movies", query);
+
+            var table = new DataTable();
+            table.Columns.Add("id");
+            table.Columns.Add("title");
+            table.Columns.Add("description");
+            table.Columns.Add("rating");
+            table.Columns.Add("genre");
+
+            foreach (var record in result.Body.Records)
+            {
+                table.Rows.Add(new[]
+                {
+                    record.Properties["id"],
+                    record.Properties["title"],
+                    record.Properties["description"],
+                    record.Properties["rating"],
+                    record.Properties["genre"]
+                });
+            }
+
+            gridView.DataSource = table;
+        }
+    }
+}
